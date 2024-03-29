4ac48659c45c7f834bd2ed376ba1a5500b4557eb
PacioTenPros <paciobumbum@gmail.com>
2024-03-29 15:27:08 +0100

    Dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 
 import { ModalProvider } from '@/providers/modal-provider';
 import { ToasterProvider } from '@/providers/toast-provider';
+import { ThemeProvider } from '@/providers/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -22,9 +23,11 @@ export default function RootLayout({
 		<ClerkProvider>
 			<html lang='en'>
 				<body className={inter.className}>
-					<ModalProvider />
-					<ToasterProvider />
-					{children}
+					<ThemeProvider attribute='class' defaultTheme='system' enableSystem>
+						<ModalProvider />
+						<ToasterProvider />
+						{children}
+					</ThemeProvider>
 				</body>
 			</html>
 		</ClerkProvider>
